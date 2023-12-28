8112a6a75bce3acf431d2c64a867a6c2a76e69f8
Samrudh Shetty <samrudh3125@gmail.com>
2023-12-28 09:26:07 +0530

    uploading files

---
+++ sum.js
@@ -0,0 +1,12 @@
+const express=require("express")
+const cors=require("cors")
+const app=express()
+app.use(cors())
+
+app.get("/sum",function(req,res){
+    const a=parseInt(req.query.a)
+    const b=parseInt(req.query.b)
+    res.send((a+b).toString())
+})
+
+app.listen(3000)
